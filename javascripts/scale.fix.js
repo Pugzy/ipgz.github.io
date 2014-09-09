45026a90b3c2a42618faded84f2181e820bf70f5
Jordan Dalton <jordan.8474@gmail.com>
2014-09-09 17:08:12 +0100

    Added a comment ;)

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,4 +1,4 @@
-var metas = document.getElementsByTagName('meta');
+var metas = document.getElementsByTagName('meta'); //Gets the element named "meta" on the HTML page
 var i;
 if (navigator.userAgent.match(/iPhone/i)) {
   for (i=0; i<metas.length; i++) {
